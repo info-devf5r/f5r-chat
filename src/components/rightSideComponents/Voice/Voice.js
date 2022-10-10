743e655151cf19409378d76b5e39246d7bdd94a8
Abbas Al Khalifa <info@devf5r.com>
2022-10-10 06:55:01 +0300

    Update Voice.js

--- src/components/rightSideComponents/Voice/Voice.js
+++ src/components/rightSideComponents/Voice/Voice.js
@@ -1,24 +1,9 @@
 import React from "react"; 
 import "./Voice.css";
 
-const Voice = ( { joinVoice, leaveVoice, join, setJoin} ) => {
-
-  const btnClick = () => {
-      if(join) { 
-        leaveVoice(); 
-        setJoin(0); 
-      } else {
-        joinVoice(); 
-        setJoin(1); 
-      }
-  }
-
   return (
     <div className="voicebox">
-      <button className="voicebutton" onClick = { btnClick }> 
-        {  join ? 
-        <p><i className="fas fa-2x fa-microphone-slash"></i></p> : <p><i className="fas fa-2x fa-microphone voiceicon"></i></p> } 
-      </button>
+
     </div>
   );
 };
