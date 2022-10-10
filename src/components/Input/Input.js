d8f524a4ba941dfab15dfa2d1d89b3c688f59618
Abbas Al Khalifa <info@devf5r.com>
2022-10-10 06:59:15 +0300

    22222

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 import './Input.css';
 
+const Voice = ( { joinVoice, leaveVoice, join, setJoin} ) => {
+
+  const btnClick = () => {
+      if(join) { 
+        leaveVoice(); 
+        setJoin(0); 
+      } else {
+        joinVoice(); 
+        setJoin(1); 
+      }
+  }
+
 const Input = ({ setMessage, sendMessage, messageToSend }) => (
   <form className="form">
+        <button className="sendButton" onClick = { btnClick }> 
+        {  join ? 
+        <p><i className="fas fa-2x fa-microphone-slash"></i></p> : <p><i className="fas fa-2x fa-microphone voiceicon"></i></p> } 
+      </button>
     <input
       className="input" 
       type="text"
