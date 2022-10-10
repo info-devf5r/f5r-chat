1953c9ec3a8576e6117dff76dd2af0048d6389bf
Abbas Al Khalifa <info@devf5r.com>
2022-10-10 05:37:39 +0300

    ZXV

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import { Outlet } from 'react-router-dom';
-import { AuthProvider } from './auth/AuthProvider';
-import Navbar from './components/Navbar';
-import './App.css';
+import React from "react"; 
 
-function App() {
-  return (
-    <AuthProvider>
-      <div className="App">
-        <Navbar className='bar' />
-        <div className='content-container'>
-          <Outlet />
-        </div>
-      </div>
-    </AuthProvider>
-  );
+import { BrowserRouter as Router, Route } from "react-router-dom"; 
+
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat"; 
+import "./main.css"
+
+import { positions, Provider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
+
+
+const options = {
+    timeout: 5000,
+    position: positions.MIDDLE
+};
+
+const App = () => { 
+    
+    return ( 
+        <Provider template={AlertTemplate} {...options}>
+            <Router> 
+                <Route path="/" exact component={Join} />
+                <Route path="/join" exact component={Join} />
+                <Route path="/chat" exact component={Chat} /> 
+            </Router>
+        </Provider>
+    ); 
 }
 
-export default App;
+export default App; 
+
+/* User enters data in "/", that data is passed to "/chat" using query-strings" */ 
