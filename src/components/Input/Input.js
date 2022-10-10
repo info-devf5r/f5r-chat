15091eb9ce7ff2ec30ad5288c81f391ac5dcb874
Abbas Al Khalifa <info@devf5r.com>
2022-10-10 07:04:52 +0300

    6

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,28 +2,6 @@ import React from 'react';
 
 import './Input.css';
 
-const Voice = ( { joinVoice, leaveVoice, join, setJoin} ) => {
-
-  const btnClick = () => {
-      if(join) { 
-        leaveVoice(); 
-        setJoin(0); 
-      } else {
-        joinVoice(); 
-        setJoin(1); 
-      }
-  }
-
-  return (
-    <div className="voicebox">
-      <button className="voicebutton" onClick = { btnClick }> 
-        {  join ? 
-        <p>Leave Voice <i className="fas fa-2x fa-microphone-slash"></i></p> : <p>Join Voice <i className="fas fa-2x fa-microphone voiceicon"></i></p> } 
-      </button>
-    </div>
-  );
-};
-
 const Input = ({ setMessage, sendMessage, messageToSend }) => (
   <form className="form">
     <input
