ba5c04d8b0a77a6d790559cce3419b4ee63e1602
Abbas Al Khalifa <info@devf5r.com>
2022-12-11 12:42:56 +0300

    Update Chat.js

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -100,7 +100,7 @@ const Chat = ({ location })=> {
 
     //const ENDPOINT = process.env.REACT_APP_API_ENDPOINT_SERVER;   // the express server 
     //const ENDPOINT = "https://116.88.240.174:5000" //process.env.REACT_APP_API_ENDPOINT_REAL; // my deployed server 
-    const ENDPOINT =  "https://server.devf5r.com/"
+    const ENDPOINT =  "http://server.devf5r.com/"
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search); 
